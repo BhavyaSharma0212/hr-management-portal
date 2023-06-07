d5fdcae36d3865bc154abcc9b274e39054dff905
Bhavya Sharma <82492348+BhavyaSharma0212@users.noreply.github.com>
2023-06-07 11:35:20 +0530

    Add files via upload

---
+++ api/Models/MasterContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models;
+
+public partial class MasterContext : DbContext
+{
+    public MasterContext()
+    {
+    }
+
+    public MasterContext(DbContextOptions<MasterContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+    //    => optionsBuilder.UseSqlServer("Data Source=LAPTOP-4GHH489T;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.DepartmentId).HasName("department_pk");
+
+            entity.ToTable("Department");
+
+            entity.Property(e => e.DepartmentName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("Employee_pk");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Department)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.EmployeeNmae)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.PhotoFileName)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
